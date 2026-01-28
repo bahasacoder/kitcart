a3ea20bc404d05b57234931104fbaabcbbf58134
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-01-28 13:10:13 +0800

    Create page.tsx
    import React from "react";
    import Navbar from "@/components/Navbar"
    import CartShop from "@/components/CartShop"
    import ProductShop from "@/components/ProductShop"
    
    
    export default function CartPage() {
      return (
        <div>
            <Navbar />
            <CartShop />
    
        </div>
      )
    }

---
+++ app/cart/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "@/components/Navbar"
+import CartShop from "@/components/CartShop"
+import ProductShop from "@/components/ProductShop"
+
+
+export default function CartPage() {
+  return (
+    <div>
+        <Navbar />
+        <CartShop />
+
+    </div>
+  )
+}
